b0d37f1b0839fe30a6746acdce8e23f06223da87
Tim <tim@congruentlabs.co>
2021-03-26 01:34:42 +1100

    change to 10k airdrop, and standardizing consts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,33 +1,44 @@
 const { BN } = require("@openzeppelin/test-helpers");
 const SATAAirdropV1 = artifacts.require("SATAAirdropV1");
 const SATAToken = artifacts.require("SATAToken");
+const {
+  pointOneEth,
+  tenThousand,
+  tenMil,
+  fortyMil,
+  tokenName,
+  tokenSymbol,
+} = require("../migrations/constants");
 
 module.exports = function (deployer) {
   var sataTokenInst;
+  const reserveAddr        = "0xce95DAde44E7307bAA616C77EF446915633dD9Ab";
+  const intAddr            = "0xC34504f0195F00914a1A3b5Adf142b015F174125";
+  const contractAddr       = "0xc441601696DF5ce0922224248AD96AB956D3B1Ae";
 
   deployer.deploy(
     SATAToken,
-    "Signata",
-    "SATA",
-    "0xce95DAde44E7307bAA616C77EF446915633dD9Ab",
-    new BN("40000000000000000000000000"), // 40,000,000
-    "0xC34504f0195F00914a1A3b5Adf142b015F174125",
-    new BN("10000000000000000000000000"), // 10,000,000
-    new BN("40000000000000000000000000"), // 40,000,000
-    { from: '0xc441601696DF5ce0922224248AD96AB956D3B1Ae', overwrite: true }
+    tokenName,
+    tokenSymbol,
+    reserveAddr,
+    new BN(fortyMil), // 40,000,000
+    intAddr,
+    new BN(tenMil), // 10,000,000
+    new BN(fortyMil), // 40,000,000
+    { from: contractAddr, overwrite: true }
   ).then((instance) => {
     sataTokenInst = instance;
     return deployer.deploy(
       SATAAirdropV1,
       SATAToken.address,
-      new BN("1000000000000000000000"), // 1,000
-      new BN("100000000000000000"), // 0.1 ETH
-      { from: '0xc441601696DF5ce0922224248AD96AB956D3B1Ae', overwrite: true }
+      new BN(tenThousand), // 10,000
+      new BN(pointOneEth), // 0.1 ETH
+      { from: contractAddr, overwrite: true }
     );
   }).then(() => {
     return sataTokenInst.mintAirdrop(
       SATAAirdropV1.address,
-      new BN("10000000000000000000000000") // 10,000,000
+      new BN(tenMil) // 10,000,000
     );
   });
 };
--- test/SATA.test.js
+++ test/SATA.test.js
@@ -4,7 +4,29 @@ const { expect } = require("chai");
 
 const SATAToken = contract.fromArtifact("SATAToken");
 const SATAAirdropV1 = contract.fromArtifact("SATAAirdropV1");
-const [ owner, claimer1, claimer2, claimer3 ] = accounts;
+const {
+  pointOneEth,
+  ninePointNineOne,
+  tenThousand,
+  tenMil,
+  fortyMil,
+  remainderBalance,
+  nintyMil,
+  hundredMil,
+  tokenName,
+  tokenSymbol,
+} = require("../migrations/constants");
+
+const [
+  owner,
+  claimer1,
+  claimer2,
+  claimer3,
+] = accounts;
+
+
+const reserveAddr        = "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0";
+const intAddr            = "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b";
 
 var token;
 var airdropContract;
@@ -12,27 +34,27 @@ var airdropContract;
 describe("Token Contract", function () {
   it("deployer is owner", async function () {
     token = await SATAToken.new(
-      "Signata",
-      "SATA",
-      "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
-      new BN("40000000000000000000000000"), // 40,000,000
-      "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
-      new BN("10000000000000000000000000"), // 10,000,000
-      new BN("40000000000000000000000000"), // 40,000,000
+      tokenName,
+      tokenSymbol,
+      reserveAddr,
+      new BN(fortyMil),
+      intAddr,
+      new BN(tenMil),
+      new BN(fortyMil),
       { from: owner })
     expect(await token.owner()).to.equal(owner);
   });
   
   it("mints the reserve address", async function() {
-    expect(await token.balanceOf("0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0")).to.be.bignumber.equal("40000000000000000000000000");
+    expect(await token.balanceOf(reserveAddr)).to.be.bignumber.equal(fortyMil);
   });
 
   it("mints the integration address", async function() {
-    expect(await token.balanceOf("0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b")).to.be.bignumber.equal("10000000000000000000000000");
+    expect(await token.balanceOf(intAddr)).to.be.bignumber.equal(tenMil);
   });
 
   it("succesfully mints the remainder", async function() {
-    expect(await token.totalSupply()).to.be.bignumber.equal("90000000000000000000000000");
+    expect(await token.totalSupply()).to.be.bignumber.equal(nintyMil);
   });
 });
 
@@ -40,31 +62,36 @@ describe("Airdrop Contract", function () {
   it("deployer is owner", async function () {
     airdropContract = await SATAAirdropV1.new(
       token.address,
-      new BN("1000000000000000000000"),
-      new BN("100000000000000000"), // 0.1 ETH
+      new BN(tenThousand),
+      new BN(pointOneEth),
       { from: owner })
     expect(await airdropContract.owner()).to.equal(owner);
   });
 
   it("can mint the airdrop", async function() {
-    expect(await token.mintAirdrop(airdropContract.address, new BN("10000000000000000000000000"), { from: owner })).to.not.throw;
+    expect(await token.mintAirdrop(airdropContract.address, new BN(tenMil), { from: owner })).to.not.throw;
   });
 
   it("has minted the airdrop", async function() {
-    expect(await token.balanceOf(airdropContract.address)).to.be.bignumber.equal("10000000000000000000000000");
-    expect(await token.totalSupply()).to.be.bignumber.equal("100000000000000000000000000"); // 100,000,000
+    expect(await token.balanceOf(airdropContract.address)).to.be.bignumber.equal(tenMil);
+    expect(await token.totalSupply()).to.be.bignumber.equal(hundredMil); // 100,000,000
   });
 
   it("can retrieve the balance of the airdrop", async function() {
-    expect(await airdropContract.availableTokens()).to.be.bignumber.equal("10000000000000000000000000");
+    expect(await airdropContract.availableTokens()).to.be.bignumber.equal(tenMil);
   });
 
   it("cannot mint the airdrop again", async function() {
-    await expectRevert(token.mintAirdrop(airdropContract.address, new BN("10000000000000000000000000"), { from: owner }), "Airdrop already minted.");
+    await expectRevert(token.mintAirdrop(
+      airdropContract.address,
+      new BN(tenMil),
+      { from: owner }
+    ), "Airdrop already minted.");
   });
 
   it("can claim airdrop with a valid account", async function() {
     expect(await airdropContract.claim({ from: claimer1 })).to.not.throw;
+    expect(await token.balanceOf(claimer1)).to.be.bignumber.equal(tenThousand);
   });
 
   it("cannot repeat a claim", async function() {
@@ -75,26 +102,25 @@ describe("Airdrop Contract", function () {
     await web3.eth.sendTransaction({
       from: claimer2,
       to: claimer1,
-      value: '9910000000000000000',
+      value: ninePointNineOne,
     });
     await expectRevert(airdropContract.claim({ from: claimer2 }), "Invalid account.");
   });
   
   it("can end the airdrop and reclaim tokens", async function() {
-    expect(await airdropContract.endAirdrop("0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0", { from: owner })).to.not.throw;
+    expect(await airdropContract.endAirdrop(reserveAddr, { from: owner })).to.not.throw;
   });
   
   it("cannot end the airdrop after airdrop depleted", async function() {
     await expectRevert(airdropContract.endAirdrop(
-      "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
+      reserveAddr,
       { from: owner }
     ), "Airdrop depleted.");
     expect(await token.balanceOf(airdropContract.address)).to.be.bignumber.equal("0");
-    expect(await token.balanceOf("0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0")).to.be.bignumber.equal("49999000000000000000000000");    
+    expect(await token.balanceOf(reserveAddr)).to.be.bignumber.equal(remainderBalance);    
   });
 
   it("cannot claim after airdrop depleted", async function() {
     await expectRevert(airdropContract.claim({ from: claimer3 }), "Airdrop depleted.");
   });
-
 });
