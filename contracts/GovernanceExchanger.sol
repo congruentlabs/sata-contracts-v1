067d20a749c4937e56ed05201297fcb4ebda533b
Ben <ben@congruentlabs.co>
2022-04-26 11:08:43 +0000

    Contract made ownable

--- contracts/GovernanceExchanger.sol
+++ contracts/GovernanceExchanger.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./extensions/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract GovernanceExchanger {
+contract GovernanceExchanger is Ownable {
     address public immutable governanceToken;
     address public immutable utilityToken;
 
@@ -26,19 +27,19 @@ contract GovernanceExchanger {
         
         require(allowance >= amount, "GovernanceExchanger::exchange: Token allowance too small.");
 
-        uint256 oldTokenBalance = IERC20(utilityToken).balanceOf(account);
+        uint256 oldTokenBalance = IERC20(utilityToken).balanceOf(msg.sender);
 
-        require(oldTokenBalance >= amount, "GovernanceExchanger::exchange: Sender's balance must be greater than the amount requested.")
-        require(IERC20(utilityToken).transferFrom(account, holdingAccount, amount), "GovernanceExchanger::exchange: Transfer of utility tokens failed.");
-        require(IERC20(governanceToken).transfer(account, amount), "GovernanceExchanger::exchange: Transfer of governance tokens failed.");
-        emit Exchanged(account, amount);
+        require(oldTokenBalance >= amount, "GovernanceExchanger::exchange: Sender's balance must be greater than or equal to the amount requested.")
+        require(IERC20(utilityToken).transferFrom(msg.sender, holdingAccount, amount), "GovernanceExchanger::exchange: Transfer of utility tokens failed.");
+        require(IERC20(governanceToken).transfer(msg.sender, amount), "GovernanceExchanger::exchange: Transfer of governance tokens failed.");
+        emit Exchanged(msg.sender, amount);
     }
 
-    function withdrawGovernance() public {
+    function withdrawGovernance() public onlyOwner {
       require(IERC20(governanceToken).transfer(holdingAccount, IERC20(governanceToken).balanceOf(address(this))), "GovernanceExchanger::withdrawGovernance: Withdraw failed.");
     }
 
-    function withdrawUtility() public {
+    function withdrawUtility() public onlyOwner {
       require(IERC20(utilityToken).transfer(holdingAccount, IERC20(utilityToken).balanceOf(address(this))), "GovernanceExchanger::withdrawUtility: Withdraw failed.");
     }
 
