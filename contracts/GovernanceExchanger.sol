5ad254c2fb99e86b7832abb2790b705f865cdcff
Tim <tim@congruentlabs.co>
2022-04-28 02:12:34 +1000

    bringing in oz tokens for single file compilation

--- contracts/GovernanceExchanger.sol
+++ contracts/GovernanceExchanger.sol
@@ -1,27 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract GovernanceExchanger is Ownable {
     address public immutable governanceToken;
     address public immutable utilityToken;
-
     address private immutable holdingAccount;
-
     uint256 private immutable endDate;
+
+    // This event is triggered whenever a call to #exchange succeeds.
+    event Exchanged(address account, uint256 amount);
 
     constructor(address governanceToken_, address utilityToken_, address holdingAccount_) {
         governanceToken = governanceToken_;
         utilityToken = utilityToken_;
         holdingAccount = holdingAccount_;
 
-        endDate = now + 14 days;
+        endDate = block.timestamp + 14 days;
     }
 
     function exchange(uint256 amount) external {
-        require(now <= endDate, "GovernanceExchanger::exchange: Exchange period has ended.");
+        require(block.timestamp <= endDate, "GovernanceExchanger::exchange: Exchange period has ended.");
         
         uint256 allowance = IERC20(utilityToken).allowance(msg.sender, address(this));
         
@@ -29,7 +30,7 @@ contract GovernanceExchanger is Ownable {
 
         uint256 utilityTokenBalance = IERC20(utilityToken).balanceOf(msg.sender);
 
-        require(utilityTokenBalance >= amount, "GovernanceExchanger::exchange: Sender's balance must be greater than or equal to the amount requested.")
+        require(utilityTokenBalance >= amount, "GovernanceExchanger::exchange: Sender's balance must be greater than or equal to the amount requested.");
         require(IERC20(utilityToken).transferFrom(msg.sender, holdingAccount, amount), "GovernanceExchanger::exchange: Transfer of utility tokens failed.");
         require(IERC20(governanceToken).transfer(msg.sender, amount), "GovernanceExchanger::exchange: Transfer of governance tokens failed.");
         emit Exchanged(msg.sender, amount);
@@ -42,7 +43,4 @@ contract GovernanceExchanger is Ownable {
     function withdrawUtility() public onlyOwner {
       require(IERC20(utilityToken).transfer(holdingAccount, IERC20(utilityToken).balanceOf(address(this))), "GovernanceExchanger::withdrawUtility: Withdraw failed.");
     }
-
-    // This event is triggered whenever a call to #exchange succeeds.
-    event Exchanged(address account, uint256 amount);
 }
